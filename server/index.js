f6fc6dd54081068303b8aee8c5a1ca7f04964111
affkjuhyt <nguyenthienlinhptit@gmail.com>
2023-12-27 20:54:47 +0700

    Add new commit

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-dotenv.config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+// dotenv.config()
 const app = express()
 
 // import routes
-const userRouter = require('../server/routes/coreRoutes/userRoutes');
+const userRouter = require('./routes/coreRoutes/userRoutes.js');
 const productRouter = require('../server/routes/coreRoutes/productRoutes')
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5002
 
 app.use(bodyParser.json())
 app.use(cors())
 
 
 //CONNECT DB
-mongoose.connect(`${process.env.MONGO_DB}`)
+mongoose.connect(`mongodb+srv://laonhi100:200145LUC@osm-shop.2wnnbil.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
         console.log('connect DB success');
     })
