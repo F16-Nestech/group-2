f38b1cca73b29d5e0a86bf084aff812762a6e797
duy <phungvanduy10a4@gmail.com>
2024-03-12 00:28:52 +0700

    order api

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -11,7 +11,118 @@ const orderController = {
         } catch (err) {
             res.status(501).json(err);
         }
+  },
+
+  // Add an order
+  addOrder: async(req, res) => {
+    console.log('add an order');
+    try {
+      const { orderItems, shippingAddress } = req.body;
+      if (!orderItems || !shippingAddress) {
+        return res.status(400).json({ message: 'Missing required fields.' });
+      }
+
+      const newOrder = new Order({
+        orderItems,
+        shippingAddress,
+      });
+
+      const savedOrder = await newOrder.save();
+
+      res.status(200).json(savedOrder);
+    } catch (err) {
+      res.status(501).json(err);
+    }
+  },
+
+  // Get an order
+  searchAnOrder: async (req, res) => {
+    console.log('get an order');
+    try {
+      // Lấy ID từ request params
+      const orderId = req.params.id;
+
+      // Kiểm tra xem ID có hợp lệ không
+      if (!mongoose.Types.ObjectId.isValid(orderId)) {
+        return res.status(400).json({ message: 'Invalid Order Id.' });
+      }
+      // Tìm đơn hàng trong cơ sở dữ liệu dựa trên ID
+      const order = await Order.findById(orderId);
+      // Kiểm tra xem đơn hàng có tồn tại không
+      if (!order) {
+        return res.status(404).json({ message: 'Order is not found.' });
+      }
+      // Trả về thông tin đơn hàng
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //Update order
+  updateOrder: async (req, res) => {
+    try {
+      const orderId = req.params.id;
+      if (!mongoose.Types.ObjectId.isValid(orderId)) {
+        return res.status(400).json({ message: 'Invalid Order Id.' });
+      }
+      
+      const updatedOrder = await Order.findByIdAndUpdate(orderId, req.body, { new: true });
+
+      if (!updatedOrder) {
+        return res.status(404).json({ message: 'Order is not found.' });
+      }
+
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //Delete Order
+  deleteOrder: async (req, res) => {
+    try {
+      const orderId = req.params.id;
+      if (!mongoose.Types.ObjectId.isValid(orderId)) {
+        return res.status(400).json({ message: 'Invalid Order Id.' });
+      }
+
+      const deletedOrder = await Order.findByIdAndDelete(orderId);
+
+      if (!deletedOrder) {
+        return res.status(404).json({ message: 'Order is not found.' });
+      }
+
+      res.status(200).json(deletedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //Delete many orders
+  deleteManyOrder: async (req, res) => {
+    try {
+      const conditions = req.body.conditions;
+      if (!conditions) {
+        return res.status(400).json({ message: 'Missing condition for delete.' });
+      }
+
+      const deletedOrders = await Order.deleteMany(conditions);
+
+      if (deletedOrders.deletedCount == 0) {
+        return res.status(404).json({ message: 'No matching order found.' });
+      }
+
+      res.status(200).json({
+        deletedCount: deletedOrders.deletedCount,
+        message: 'Orders successfully deleted.'
+      });
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err);
+    }
   }
+
 }
 
 module.exports = orderController;
--- server/routes/coreRoutes/orderRoutes.js
+++ server/routes/coreRoutes/orderRoutes.js
@@ -4,22 +4,22 @@ const orderController = require("../../controllers/orderController");
 
 const router = express.Router();
 
-// add a order
-// router.post("/create");
+// add an order
+router.post("/create", orderController.addOrder);
 
 // get all orders
 router.get("/list", orderController.getOrders);
 
 // get an order
-// router.get("/read-order/:id");
+router.get("/read-order/:id", orderController.searchAnOrder);
 
 // update order
-// router.put("/update/:id");
+router.put("/update/:id", orderController.updateOrder);
 
 // delete order
-// router.delete("/delete/:id");
+router.delete("/delete/:id", orderController.deleteOrder);
 
 // delete many order
-// router.delete("/delete-many");
+router.delete("/delete-many", orderController.deleteManyOrder);
 
 module.exports = router;
